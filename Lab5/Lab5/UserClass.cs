8bc9551ec0bcc2283b91787e0666e7d7e6a35ea5
Oscar Wolter <Oscardawolter@gmail.com>
2017-10-20 13:56:16 +0200

    Added all elements and gave them identifying names.

---
+++ Lab5/Lab5/UserClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    class UserClass
+    {
+        private string UserName { get; set; }
+        private string UserEmail { get; set; }
+        private bool IsAdmin { get; set; }
+
+        public UserClass(string userName, string userEmail, bool isAdmin = false)
+        {
+            this.UserName = userName;
+            this.UserEmail = userEmail;
+            this.IsAdmin = isAdmin;
+        }
+    }
+}
