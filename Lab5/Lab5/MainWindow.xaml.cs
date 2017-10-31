846eed896480f3f3671ee2fcb0dd01ce1aac2cf0
Superbrain1337 <31820507+Superbrain1337@users.noreply.github.com>
2017-10-31 13:40:58 +0100

    Last tweaks

--- Lab5/Lab5/MainWindow.xaml.cs
+++ Lab5/Lab5/MainWindow.xaml.cs
@@ -39,18 +39,7 @@ namespace Lab5
                 ListBox_UserList.Items.Clear();
 
                 //Add user to list users
-                //if (IsEmail(TextBox_CreateUserEmail.Text))
-                //Index = ListBox_UserList.SelectedIndex;
-                //if (Index != -1)
-                //{
-                //    Users[Index] = new UserClass(TextBox_CreateUserName.Text, TextBox_CreateUserEmail.Text);
-                //}
-                //else
-                //{
-                    Users.Add(new UserClass(TextBox_CreateUserName.Text, TextBox_CreateUserEmail.Text));
-                //}
-                
-                //Sort by name
+                Users.Add(new UserClass(TextBox_CreateUserName.Text, TextBox_CreateUserEmail.Text));
                 
                 //Add the new users lst to listbox
                 for (int i = 0; i < Users.Count; i++)
@@ -69,10 +58,12 @@ namespace Lab5
         
         private void ListBox_UserList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            //If something is selected in AdminList i gets unselected
             if (ListBox_AdminList.SelectedIndex != -1)
             {
                 ListBox_AdminList.SelectedIndex = -1;
             }
+            //The userInfo i displayed
             Icon_UserIcon_Placeholder.Opacity = 100;
             Index = ListBox_UserList.SelectedIndex;
             if (Index != -1)
@@ -83,17 +74,21 @@ namespace Lab5
             }
             else
             {
+                Button_CreateAdmin.IsEnabled = false;
+                Button_RemoveUser.IsEnabled = false;
                 Label_UserInfo.Content = "Namn: \nEmail:";
             }
         }
 
         private void ListBox_AdminList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            //If something is selected in UserList it gets unselected
             if (ListBox_UserList.SelectedIndex != -1)
             {
                 ListBox_UserList.SelectedIndex = -1;
                 Button_CreateAdmin.IsEnabled = false;
             }
+            //Displays the UserInfo
             Icon_UserIcon_Placeholder.Opacity = 100;
             Index = ListBox_AdminList.SelectedIndex;
             if (Index != -1)
@@ -104,6 +99,8 @@ namespace Lab5
             }
             else
             {
+                Button_RemoveAdmin.IsEnabled = false;
+                Button_RemoveUser.IsEnabled = false;
                 Label_UserInfo.Content = "Namn: \nEmail:";
             }
         }
@@ -160,6 +157,7 @@ namespace Lab5
 
         private void Button_CreateAdmin_Click(object sender, RoutedEventArgs e)
         {
+            //The selected user is added to the AdminList and removed from the UserList
             Index = ListBox_UserList.SelectedIndex;
             if (Index != -1)
             {
@@ -175,6 +173,7 @@ namespace Lab5
 
         private void Button_RemoveAdmin_Click(object sender, RoutedEventArgs e)
         {
+            //The selected admin is removed from the AdminList and added to the UserList
             Index = ListBox_AdminList.SelectedIndex;
             if (Index != -1)
             {
@@ -192,6 +191,7 @@ namespace Lab5
         {
             int userIndex = ListBox_UserList.SelectedIndex;
             int adminIndex = ListBox_AdminList.SelectedIndex;
+            //Checks which list was last used and removes the selected user/admin
             if (lastUsedList == 0)
             {
                 if (userIndex != -1)
@@ -200,6 +200,7 @@ namespace Lab5
                         ListBox_UserList.Items.RemoveAt(userIndex);
                         Users.Remove(theUser);
                 }
+                Button_CreateAdmin.IsEnabled = false;
             }
             else if (lastUsedList == 1)
             {
@@ -218,6 +219,7 @@ namespace Lab5
         
         public bool IsEmail(string email)
         {
+            //Checks if whats typed is an email
             string pattern1 = @".+@.+[.].{2,3}";
             if (Regex.IsMatch(email, pattern1))
             {
@@ -296,6 +298,7 @@ namespace Lab5
 
         private void Button_EditUser_Click(object sender, RoutedEventArgs e)
         {
+            //Updates the values of the selected index to the info in the textboxes
             Index = ListBox_UserList.SelectedIndex;
             if (Index != -1)
             {
@@ -319,7 +322,7 @@ namespace Lab5
                     Admins[Index].UserEmail = TextBox_CreateUserEmail.Text;
                 }
                 ListBox_AdminList.Items.RemoveAt(Index);
-                ListBox_AdminList.Items.Insert(tempIndex, Users[tempIndex].UserName);
+                ListBox_AdminList.Items.Insert(tempIndex, Admins[tempIndex].UserName);
             }
             TextBox_CreateUserName.Clear();
             TextBox_CreateUserEmail.Clear();
